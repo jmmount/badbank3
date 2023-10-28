4256ec89772dec3be206b7797f7ca3b485224432
jmmount <eslnyc.jason@gmail.com>
2023-10-28 15:11:59 -0400

    updates  navbar btn

--- public/navbar.js
+++ public/navbar.js
@@ -1,6 +1,5 @@
-function NavBar(){
-  return(
-
+function NavBar() {
+  return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <a className="navbar-brand" href="#">BadBank</a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -10,9 +9,6 @@ function NavBar(){
         <ul className="navbar-nav">
           <li className="nav-item">
             <a className="nav-link" href="#/CreateAccount/">Create Account</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#/login/">Login</a>
           </li>
           <li className="nav-item">
             <a className="nav-link" href="#/deposit/">Deposit</a>
@@ -25,10 +21,25 @@ function NavBar(){
           </li>
           <li className="nav-item">
             <a className="nav-link" href="#/alldata/">AllData</a>
-          </li>          
+          </li>
+        </ul>
+        <ul className="navbar-nav ml-auto" id="login-menu"> {/* Use ml-auto to move the "Login" item to the right */}
+        <li className="nav-item">
+    <button className="btn btn-link nav-button" onClick={() => window.location.href = '#/login/'}>
+      Login
+    </button>
+  </li>
         </ul>
       </div>
+      <style>
+        {`
+          #login-menu .nav-button {
+            border: 1px solid #007BFF;
+            border-radius: 5px;
+            padding: 5px 15px;
+          }
+        `}
+      </style>
     </nav>
-
   );
 }
