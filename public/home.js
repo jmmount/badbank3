5541b1ec8fd96302b4b2757142c8d3a385b2568e
jmmount <eslnyc.jason@gmail.com>
2023-10-29 17:05:29 -0400

    updates  msg

--- public/home.js
+++ public/home.js
@@ -3,7 +3,7 @@ function Home(){
     <Card
       txtcolor="black"
       header="BadBank Landing Module"
-      title="Welcome to the bank"
+      title="Login or create an account to get started."
       text="You can move around using the navigation bar."
       body={(<img src="bank.png" className="img-fluid" alt="Responsive image"/>)}
     />
