c63b34f3576a0b3d447cbfa1603e7d7196859153
jmmount <eslnyc.jason@gmail.com>
2023-10-29 22:37:04 -0400

    updates  deposit and withdraw

--- public/withdraw.js
+++ public/withdraw.js
@@ -1,75 +1,85 @@
-function Withdraw(){
-  const [show, setShow]     = React.useState(true);
-  const [status, setStatus] = React.useState('');  
+function Withdraw() {
+  const [show, setShow] = React.useState(true);
+  const [status, setStatus] = React.useState('');
+  const [loggedInUser, setLoggedInUser] = React.useState(JSON.parse(localStorage.getItem('user')));
+
+  const updateUserBalance = (newBalance) => {
+    setLoggedInUser(prevUser => ({ ...prevUser, balance: newBalance }));
+  };
 
   return (
     <Card
       bgcolor="primary"
       header="Withdraw"
-      status={status}
-      body={show ? 
-        <WithdrawForm setShow={setShow} setStatus={setStatus}/> :
-        <WithdrawMsg setShow={setShow} setStatus={setStatus}/>}
+      body={
+        show ? (
+          <WithdrawForm
+            setShow={setShow}
+            setStatus={setStatus}
+            user={loggedInUser}
+            updateUserBalance={updateUserBalance}
+          />
+        ) : (
+          <WithdrawMsg user={loggedInUser} setShow={setShow} setStatus={setStatus} />
+        )
+      }
     />
-  )
+  );
 }
 
-function WithdrawMsg(props){
-  return(<>
-    <h5>Success</h5>
-    <button type="submit" 
-      className="btn btn-light" 
-      onClick={() => {
-        props.setShow(true);
-        props.setStatus('');
-      }}>
+function WithdrawMsg(props) {
+  return (
+    <>
+      <h5>Success {props.user.name}, your new balance is {props.updateUserBalance} dollars</h5>
+      <button
+        type="submit"
+        className="btn btn-light"
+        onClick={() => {
+          props.setShow(true);
+          props.setStatus('');
+        }}>
         Withdraw again
-    </button>
-  </>);
+      </button>
+    </>
+  );
 }
 
-function WithdrawForm(props){
-  const [email, setEmail]   = React.useState('');
+function WithdrawForm(props) {
   const [amount, setAmount] = React.useState('');
 
-  function handle(){
-    fetch(`/account/update/${email}/-${amount}`)
-    .then(response => response.text())
-    .then(text => {
+  function handle() {
+    fetch(`/account/update/${props.user.email}/-${amount}`)
+      .then(response => response.text())
+      .then(text => {
         try {
-            const data = JSON.parse(text);
-            props.setStatus(`Your new balance is: $${data.value.balance}`);
-            props.setShow(false);
-            console.log('JSON:', data);
-        } catch(err) {
-            props.setStatus('Deposit failed')
-            console.log('err:', text);
+          const data = JSON.parse(text);
+          props.setStatus(JSON.stringify(data.value));
+          props.setShow(false);
+          props.updateUserBalance(data.newBalance); // Update user balance after successful withdrawal
+          console.log('JSON:', data);
+        } catch (err) {
+          props.setStatus('Withdrawal failed');
+          console.log('err:', text);
         }
-    });
+      });
   }
 
+  return (
+    <>
+      Welcome Back: {props.user.name} <br />
+      Amount<br />
+      <input
+        type="number"
+        className="form-control"
+        placeholder="Enter amount"
+        value={amount}
+        onChange={e => setAmount(e.currentTarget.value)}
+      />
+      <br />
 
-  return(<>
-
-    Email<br/>
-    <input type="input" 
-      className="form-control" 
-      placeholder="Enter email" 
-      value={email} 
-      onChange={e => setEmail(e.currentTarget.value)}/><br/>
-
-    Amount<br/>
-    <input type="number" 
-      className="form-control" 
-      placeholder="Enter amount" 
-      value={amount} 
-      onChange={e => setAmount(e.currentTarget.value)}/><br/>
-
-    <button type="submit" 
-      className="btn btn-light" 
-      onClick={handle}>
+      <button type="submit" className="btn btn-light" onClick={handle}>
         Withdraw
-    </button>
-
-  </>);
+      </button>
+    </>
+  );
 }
