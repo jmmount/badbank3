58086a60068516699c38fb3f65679a0dd2e5389a
jmmount <eslnyc.jason@gmail.com>
2023-11-01 19:53:57 -0400

    adds logo

--- public/home.js
+++ public/home.js
@@ -2,10 +2,10 @@ function Home(){
   return (
     <Card
       txtcolor="black"
-      header="BadBank Landing Module"
+      header="Welcome to BadBank3"
       title="Login or create an account to get started."
       text="You can move around using the navigation bar."
-      body={(<img src="bank.png" className="img-fluid" alt="Responsive image"/>)}
+      body={(<img src="bank.png" className="img-fluid image-bigger" alt="Responsive image"/>)}
     />
   );  
 }
